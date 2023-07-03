e442d5359c603fab3e5c827781e0e5c2366d9b83
RasikaRD <rasikarajith94@gmail.com>
2023-07-03 08:54:21 +0530

    transaction list scroller

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,7 +3,6 @@ import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
-  
   final List<Transaction> transaction;
   const TransactionList(this.transaction);
 
@@ -11,54 +10,55 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 400,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transaction.map((tx) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    width: 100,
-                    margin: const EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 10,
-                    ),
-                    padding: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                      color: Colors.deepPurple,
-                      width: 2,
-                    )),
-                    child: Text(
-                      '\$${tx.amount}',
-                      style: const TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.deepPurple),
-                    ),
+      child: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: 100,
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 10,
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(tx.title,
-                          style: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          )),
-                      Text(DateFormat('yyyy-MM-dd').format(tx.date),
-                          style: const TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.black,
-                          )),
-                    ],
-                  )
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                    color: Colors.deepPurple,
+                    width: 2,
+                  )),
+                  child: Text(
+                    '\$${transaction[index].amount}',
+                    style: const TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.deepPurple),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(transaction[index].title,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        )),
+                    Text(
+                        DateFormat('yyyy-MM-dd')
+                            .format(transaction[index].date),
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                        )),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: transaction.length,
       ),
     );
   }
